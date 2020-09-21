91d36841f4270196d0b4599f590084bb6488db24
luke <luke1582@users.noreply.github.com>
2020-09-21 15:05:35 +0800

    Initial commit

---
+++ project1.py
@@ -0,0 +1,16 @@
+m = input('请输入一个奇数')
+if int(m) % 2 == 0:
+    print("宁输错了")
+else:
+    i = 0
+    while i < int(m):
+        j = 0
+        while j < int(m) - i - 1:
+            print(' ',end="")
+            j = j + 1
+        j = 0
+        while j < 1 + 2 * i:
+            print('*',end="")
+            j=j+1
+        print("\n",end="")
+        i = i + 1
